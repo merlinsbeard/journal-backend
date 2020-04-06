a54ba83468e909af3e7693cc936d409f22b90e94
Ben <bjpaat01@gmail.com>
2020-04-06 17:04:08 +0800

    Sort posts by date updated
    Signed-off-by: Ben <bjpaat01@gmail.com>

--- notebook/views.py
+++ notebook/views.py
@@ -1,11 +1,11 @@
 from .serializers import PageSerializer, CategorySerializer
 from .models import Page, PageCategory, Category
-from rest_framework import generics
+from rest_framework import generics, viewsets, mixins
 from rest_framework.permissions import IsAuthenticated
 
 
 class PageList(generics.ListCreateAPIView):
-    queryset = Page.objects.filter(is_active=True)
+    queryset = Page.objects.filter(is_active=True).order_by('-date_updated')
     serializer_class = PageSerializer
 
 
@@ -15,6 +15,16 @@ class PageDetail(generics.RetrieveUpdateAPIView):
     lookup_field = "slug"
 
 
-class CategoryList(generics.ListAPIView):
+class CategoryList(generics.ListAPIView, generics.CreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+
+# Ability to make tags
+# Ability to add tags
+
+class PageViewSet(viewsets.ModelViewSet):
+    queryset = Page.objects.filter(is_active=True).order_by('-date_updated')
+    serializer_class = PageSerializer
+    lookup_field = 'slug'
