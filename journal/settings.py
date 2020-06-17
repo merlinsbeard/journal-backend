5d2f4bfecea87f3c8d949cf42dcbc510009229b6
Ben <me@benpaat.xyz>
2020-06-17 22:22:45 +0800

    📦 Update Django package into 3
    Removed unued jwt package its not maintained anymore
    
    Updated other packages to be compatible with django3

--- journal/settings.py
+++ journal/settings.py
@@ -145,15 +145,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ),
-}
-JWT_AUTH = {
-    'JWT_ALLOW_REFRESH': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
-    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7)
 }
 
 MARKDOWN_EXTRAS = [
